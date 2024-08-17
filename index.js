c946af08e2a32828493bf0e98a4cfc07d7ade7c0
CelineMali <seveno.celine@gmail.com>
2024-08-17 17:45:40 +0200

    3.6 end

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
 let notes = [
-  { id: "1", content: "HTML is easy", important: true },
-  { id: "2", content: "Browser can execute only JavaScript", important: false },
   {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
+    id: 1,
+    content: "HTML is easy",
     important: true,
   },
   {
-    id: "4",
-    content:
-      "nodemon is dev dependency used to watch a file and reload on change. See script for command",
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
 ];
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
 };
 
-// get all notes
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
-// get a single note
-app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.statusMessage = "No note found";
-    response.status(404).end();
-  }
-});
-//delete
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-  response.status(204).end();
-});
-
-//add
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
@@ -61,7 +58,7 @@ app.post("/api/notes", (request, response) => {
 
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
+    important: body.important || false,
     id: generateId(),
   };
 
@@ -70,6 +67,27 @@ app.post("/api/notes", (request, response) => {
   response.json(note);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    console.log("x");
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
+
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
